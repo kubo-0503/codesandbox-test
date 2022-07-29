7454da97d37730dc70404e7a0d2fe0861aa68a02
kubo <okubo_syukatu@yahoo.co.jp>
2022-07-29 14:48:15 +0000

    Update 5 files

--- src/index.js
+++ src/index.js
@@ -1,10 +1,3 @@
-import "./styles.css";
+const name = "大久保雅子";
 
-document.getElementById("app").innerHTML = `
-<h1>Hello Vanilla!</h1>
-<div>
-  We use the same configuration as Parcel to bundle this sandbox, you can find more
-  info about Parcel 
-  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
-</div>
-`;
+const nameMSG = `私の名前は${name}`;
